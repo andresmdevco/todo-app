961a3fcc4444c421405b69ee7cf1b41b5b78dca4
Andrés Muñoz <andres2078@gmail.com>
2026-04-22 23:09:04 -0500

    feat: add todo store methods skeleton

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -22,7 +22,52 @@ const initStore = () => {
     console.log('InitStore 🥑');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+const setFilter = (newFilter = Filters.All) => {
+    throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
+
+
+
 
 export default {
-    initStore
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    initStore,
+    loadStore,
+    setFilter,
+    toggleTodo
 }
