6841af24b8cb26587bd638fc5ed5c0510246fdea
Andrés Muñoz <andres2078@gmail.com>
2026-04-24 22:44:56 -0500

    feat: add pending todos counter

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,12 +1,13 @@
 import html from "./app.html?raw";
 import todoStore, { Filters } from "../store/todo.store";
-import { renderTodos } from './use-cases';
+import { renderTodos, renderPending } from './use-cases';
 
 const ElementIDs = {
     ClearCompletedButton: '.clear-completed',
     TodoList: '.todo-list', 
     NewTodoInput: '#new-todo-input',
-    TodoFilters: '.filtro'
+    TodoFilters: '.filtro',
+    PendingCountLabel: '#pending-count'
 }
 
 /**
@@ -19,6 +20,11 @@ export const App = (elementId) => {
     const displayTodos = () => {
         const todos = todoStore.getTodos(todoStore.getCurrentFilter());
         renderTodos(ElementIDs.TodoList, todos);
+        updatePendingCount();
+    }
+
+    const updatePendingCount = () => {
+        renderPending(ElementIDs.PendingCountLabel);
     }
 
     // Cuando la función App() se llama
@@ -86,4 +92,7 @@ export const App = (elementId) => {
             displayTodos();
         });
     });
+
+
+
 }
